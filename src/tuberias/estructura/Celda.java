e13eaeb23c5ca49f7c1183abd692365aee862e0f
Dard0 <contacto@dard0.es>
2014-10-25 15:39:08 +0200

    TDS PTS

--- src/tuberias/estructura/Celda.java
+++ src/tuberias/estructura/Celda.java
@@ -1,8 +1,12 @@
 package tuberias.estructura;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Celda {
-	private Celda[] vecinas;
-	private static final int CAPACIDAD = 5;
+	private Map<Direccion, Celda> vecinas;
+	private int capacidad;
+	private final static int CAPACIDAD_MAXIMA = 5;
 	private int caudal;
 	private boolean saturada;
 	private boolean vacia;
@@ -10,17 +14,30 @@ public class Celda {
 	//CONSTRUCTORES
 	public Celda(){
 		this.caudal = 0;
+		this.capacidad = CAPACIDAD_MAXIMA;
 		this.saturada = false;
 		this.vacia = true;
+		this.vecinas = new HashMap<Direccion, Celda>();
 	}
 
+	public Celda(int capacidad){
+		this.caudal = 0;
+		this.capacidad = capacidad;
+		this.saturada = false;
+		this.vacia = true;
+		this.vecinas = new HashMap<Direccion, Celda>();
+		
+		//T_D_S P_T_S
+		// vecinas.put(Direccion.DERECHA, new Celda(23));
+	}
+	
 	//GETTERS
-	public Celda[] getVecinas() {
-		return vecinas.clone();
+	public Map<Direccion,Celda> getVecinas() {
+		return new HashMap<Direccion,Celda>(vecinas);
 	}
 
-	public static int getCapacidad() {
-		return CAPACIDAD;
+	public int getCapacidad() {
+		return capacidad;
 	}
 
 	public int getCaudal() {
@@ -37,12 +54,15 @@ public class Celda {
 	
 	//FUNCIONALIDAD
 	public void incrementarCaudal(){
-		if (this.caudal+1 <= CAPACIDAD) this.caudal++;
+		if (this.caudal+1 <= CAPACIDAD_MAXIMA) this.caudal++;
 	}
 	public void decrementarCaudal(){
 		if (this.caudal-1 >= 0) this.caudal--;
 	}
 	
+	public void establecerVecina(Celda celda, Direccion direccion){
+		
+	}
 	
 
 
